8a5733a1cf027630f366dd8d6ebac0e0b0fe998f
Greg Kreisman <gkreisman_cep@qq.com>
2020-10-25 19:21:03 +0100

    Update euler9.py

--- euler9.py
+++ euler9.py
@@ -1,4 +1,4 @@
-def diaphant(m,n):
+def diophant(m,n):
     ## m is larger
     a = m**2 - n**2
     b = 2*(m*n)
@@ -6,13 +6,13 @@ def diaphant(m,n):
     return a, b, c
 
 
-print(diaphant(2,1))
+print(diophant(2,1))
 
-print(sum(diaphant(2,3)))
+print(sum(diophant(2,3)))
 
 for mm in range(1,500):
     for nn in range(1,500):
-        aa,bb,cc = diaphant(mm,nn)
+        aa,bb,cc = diophant(mm,nn)
         if aa+bb+cc == 1000 and aa >0 and bb>0 and cc>0: ##will have neg sol too 
             print("p trp that adds up to 1000 is: a, b, c: ",aa,bb,cc)
             print("the product of a, b and c is: ",aa*bb*cc)  
