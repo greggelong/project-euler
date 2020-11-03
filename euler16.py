c107bbad511f4e84ada653a47362d8700e30c9cf
Greg Kreisman <gkreisman_cep@qq.com>
2020-11-03 19:47:27 +0800

    Add files via upload

---
+++ euler16.py
@@ -0,0 +1,13 @@
+a = 2**1000
+aa = str(a)
+
+total =0
+
+for i in aa:
+	total = total + int(i)
+
+
+print("the sum of the digets in 2^1000 is: ", total)
+
+
+#the sum of the digets in 2^1000 is:  1366
